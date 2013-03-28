cfe09b5b232e7e4dbd2ffc966645051432fd51bb
James Childers <james.childers@gmail.com>
2013-03-28 09:45:10 -0500

    Add secret_token.rb to .gitignore

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Mcmodder::Application.config.secret_token = 'ce08b956e30d721b825f3494282e81fc1e03cc26a5321df1f6dc59c02a371318f62678bef29f355f6b11430010beceabb8f4e35fac364a4ab17d22608a5d4ff7'
